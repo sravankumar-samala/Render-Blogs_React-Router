d42310fd98bf643401e5e2986f4bc4dcfbfc66e8
sravankumar-samala <srravankumarr@gmail.com>
2023-05-20 06:24:40 +0000

    Initial COmmit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+/* eslint-disable import/no-unresolved */
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
+import Header from './components/Header'
+import About from './components/About'
+import BlogsList from './components/BlogsList'
+import BlogItemDetails from './components/BlogItemDetails'
+// eslint-disable-next-line import/extensions
+import Contact from './components/Contact'
+import NotFound from './components/NotFound'
+
+import './App.css'
+
+const App = () => (
+  <BrowserRouter>
+    <Header />
+    <Switch>
+      <Route exact path="/" component={BlogsList} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/contact" component={Contact} />
+      <Route exact path="/blogs/:id" component={BlogItemDetails} />
+      <Route component={NotFound} />
+    </Switch>
+  </BrowserRouter>
+)
+
+export default App
---
+++ src/components/BlogItemDetails/index.js
@@ -0,0 +1,62 @@
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
+import './index.css'
+
+class BlogItemDetails extends Component {
+  state = {blogDetails: []}
+
+  componentDidMount = () => {
+    this.getBlogItemData()
+  }
+
+  getBlogItemData = async () => {
+    const {match} = this.props
+    const {params} = match
+    const {id} = params
+
+    const response = await fetch(`https://apis.ccbp.in/blogs/${id}`)
+    const data = await response.json()
+
+    const updatedData = {
+      id: data.id,
+      author: data.author,
+      avatarUrl: data.avatar_url,
+      imageUrl: data.image_url,
+      content: data.content,
+      title: data.title,
+      topic: data.topic,
+    }
+
+    this.setState({blogDetails: updatedData})
+    // console.log(updatedData)
+  }
+
+  renderBlogItemDetails = () => {
+    const {blogDetails} = this.state
+    const {title, imageUrl, content, avatarUrl, author} = blogDetails
+    return blogDetails.length === 0 ? (
+      <Loader type="TailSpin" color="#00BFFF" height={50} width={50} />
+    ) : (
+      <div className="blog-info">
+        <h2 className="blog-details-title">{title}</h2>
+
+        <div className="author-details">
+          <img className="author-pic" src={avatarUrl} alt={author} />
+          <p className="details-author-name">{author}</p>
+        </div>
+
+        <img className="blog-image" src={imageUrl} alt={title} />
+        <p className="blog-content">{content}</p>
+      </div>
+    )
+  }
+
+  render() {
+    return <div className="blog-container">{this.renderBlogItemDetails()}</div>
+  }
+}
+
+export default BlogItemDetails
